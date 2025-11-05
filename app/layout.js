d75790a06671a9f4dd7f5185c92754f67a7dac44
zubsaif <zubsaif@gmail.com>
2025-11-05 03:01:18 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'DC Load Adder Pro',
+  description: 'Telecom Power Budget Calculator',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
